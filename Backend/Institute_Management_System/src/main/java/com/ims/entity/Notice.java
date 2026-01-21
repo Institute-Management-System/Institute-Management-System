b4524c2357f41da2c7c684a5fc5a06fc6820959b
dj107 <dheerajghosh007@gmail.com>
2026-01-21 13:34:03 +0530

    notice entity added

--- Backend/Institute_Management_System/src/main/java/com/ims/entity/Notice.java
+++ Backend/Institute_Management_System/src/main/java/com/ims/entity/Notice.java
@@ -1,5 +1,30 @@
 package com.ims.entity;
 
-public class Notice {
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
+@Entity
+@Table(name = "notices")
+@AttributeOverride(
+    name = "id",
+    column = @Column(name = "notice_id")
+)
+public class Notice extends BaseEntity {
+
+    @Column(name = "title", length = 200)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "publish_date")
+    private LocalDate publishDate;
+
+    //@Enumerated(EnumType.STRING)
+    //@Column(name = "target_role")
+    //private Role targetRole;
+
+
+    }
 }
+
