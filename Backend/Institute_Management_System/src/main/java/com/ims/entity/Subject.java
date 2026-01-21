1beb13f49a9db8f8e9b0448a773ad3989b8b6641
pimpalkarprathamesh <prathmeshpim2002@gmail.com>
2026-01-21 14:30:52 +0530

    subject entity added

--- Backend/Institute_Management_System/src/main/java/com/ims/entity/Subject.java
+++ Backend/Institute_Management_System/src/main/java/com/ims/entity/Subject.java
@@ -1,5 +1,45 @@
 package com.ims.entity;
 
-public class Subject {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//Lombok Annotations
+@Getter
+@Setter
+@AttributeOverride(name="id",column = @Column(name="subject_id"))
+
+//JPA Annotations
+@Entity
+@Table(name="subjects")
+public class Subject {
+		
+	@Column(name = "name", nullable = false, length = 100)
+	private String name;
+	
+	@Column(name="description", length=500)
+	private String description;
+	
+	// Many subjects belong to one course
+	//@ManyToMany(fetch = FetchType.LAZY)
+        //@JoinColumn(name="course_id") // FK column in subjects table
+	//private Long courseId;
+	
+	// inserted_by -> user who inserted this subject (Admin/Teacher)
+	//@ManyToMany(fetch = FetchType.LAZY)
+        //@JoinColumn(name="inserted_by") // FK column in subjects table
+	private Long insertedBy;
+	
+	//@Column(name="status"
+	//private STATUS status;
+	
+	// for soft delete
+	@Column(name="deleted_at")
+	private LocalDateTime deletedAt;
+	
 }
