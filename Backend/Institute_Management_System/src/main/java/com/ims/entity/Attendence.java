38603c479b7046c3ac65fce6081276167a049938
Teerth63064 <teerthbudhauliya0@gmail.com>
2026-01-21 15:56:52 +0530

    attendence entity added

--- Backend/Institute_Management_System/src/main/java/com/ims/entity/Attendence.java
+++ Backend/Institute_Management_System/src/main/java/com/ims/entity/Attendence.java
@@ -1,6 +1,32 @@
-package com.ims.entity; 
+package com.ims.entity;
 
+import java.time.LocalDate;
 
-public class Attendence {
-    
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@AttributeOverride(name = "id", column = @Column(name = "attendance_id"))
+
+@Entity
+@Table(name = "attendance")
+public class Attendence extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+    @ManyToOne
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+    @Column(name = "attendance_date", nullable = false)
+    private LocalDate attendanceDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
