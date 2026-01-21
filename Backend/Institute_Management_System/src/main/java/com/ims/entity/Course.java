923ef3cc8850c4aad8d2f0409dd2427d8911926d
dj107 <dheerajghosh007@gmail.com>
2026-01-21 13:23:31 +0530

    Course entity added

--- Backend/Institute_Management_System/src/main/java/com/ims/entity/Course.java
+++ Backend/Institute_Management_System/src/main/java/com/ims/entity/Course.java
@@ -1,5 +1,23 @@
 package com.ims.entity;
 
-public class Course {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "courses")
+@AttributeOverride(
+    name = "id",
+    column = @Column(name = "course_id")
+)
+public class Course extends BaseEntity {
+
+    private String name;
+
+    @Column(name = "duration_months")
+    private Integer durationMonths;
+
+    private String description;
+
+    @Column(name = "timetable_path")
+    private String timetablePath;
 }
+
