ce0f77a0e1a0ca3e2384762d7ec9421be80e245b
Teerth63064 <teerthbudhauliya0@gmail.com>
2026-01-21 16:09:25 +0530

    fee entity added

--- Backend/Institute_Management_System/src/main/java/com/ims/entity/Fee.java
+++ Backend/Institute_Management_System/src/main/java/com/ims/entity/Fee.java
@@ -1,5 +1,36 @@
 package com.ims.entity;
 
-public class Fee {
- 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@AttributeOverride(name = "id", column = @Column(name = "fee_id"))
+
+@Entity
+@Table(name = "fees")
+public class Fee extends BaseEntity {
+
+	@ManyToOne
+	@JoinColumn(name = "student_id")
+	private User student;
+
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
+	@Column(precision = 10, scale = 2)
+	private BigDecimal amount;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private FeeStatus status;
+	@Column(name = "payment_date")
+	private LocalDate paymentDate;
+	@Column(name = "is_inactive")
+	private boolean inactive;
 }
---
+++ Backend/Institute_Management_System/src/main/java/com/ims/entity/FeeStatus.java
@@ -0,0 +1,7 @@
+package com.ims.entity;
+
+public enum FeeStatus {
+	PAID,
+    PENDING,
+    OVERDUE
+}
