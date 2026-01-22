9b98facccf3645621b076b25769e12ef2a9204e5
Teerth63064 <teerthbudhauliya0@gmail.com>
2026-01-22 13:37:06 +0530

    attendence service added

--- Backend/Institute_Management_System/src/main/java/com/ims/repository/AttendenceRepository.java
+++ Backend/Institute_Management_System/src/main/java/com/ims/repository/AttendenceRepository.java
@@ -4,18 +4,32 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.ims.dto.AttendanceTableResponse;
 import com.ims.entity.Attendence;
 import com.ims.entity.Status;
 
 public interface AttendenceRepository extends JpaRepository<Attendence, Long>{
 	
-	  // List table detail
-    List<Attendence> findByUserIdAndAttendanceDateBetween(
-            Long userId,
-            LocalDate startDate,
-            LocalDate endDate
-    );
+	 @Query("""
+		        SELECT new com.ims.dto.AttendanceTableResponse(
+		            a.attendanceDate,
+		            a.status,
+		            a.subject.name
+		        )
+		        FROM Attendence a
+		        WHERE a.user.id = :userId
+		        AND a.attendanceDate BETWEEN :startDate AND :endDate
+		        ORDER BY a.attendanceDate DESC
+		    """)
+
+     List<AttendanceTableResponse> findAttendanceTableData(
+		            @Param("userId") Long userId,
+		            @Param("startDate") LocalDate startDate,
+		            @Param("endDate") LocalDate endDate
+		    );
 
     // Total lectures
     long countByUserIdAndCourseIdAndAttendanceDateBetween(
